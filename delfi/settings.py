e8a9b731d277afc1caaa34beece6c6d5afac65c2
Aidis Stukas <aidiss@gmail.com>
2019-01-30 20:26:11 +0200

    Try fixing ssl error

--- delfi/settings.py
+++ delfi/settings.py
@@ -9,3 +9,4 @@ NEWSPIDER_MODULE = 'delfi.spiders'
 USER_AGENT = 'Friendly scraper'
 DOWNLOAD_DELAY = 1
 ITEM_PIPELINES = {'delfi.pipelines.DelfiPipeline': 300}
+DOWNLOADER_CLIENT_TLS_METHOD = 'TLSv1.2'
